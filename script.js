3cba197d16fe00553ba2f89223e12a2f5f53d324
Noah-Jarzab <njarzab721@gmail.com>
2020-07-10 10:51:59 -0600

    added psuedocode to js file

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Use DOM manipulation to populate the 2 letters sections with available letters and then picked letters
+// Receive input from the .phrase box with the submit button to create blank spaces under the image that starts out empty
+// create a timer in the corner that starts when the start button is clicked
+// create a start button after the phrase is received to start the timer
+// create a score and score function based on time remaining once the phrase is completed
+// create a function to check the phrase and end the game upon completion
+// allow the score to carry over into the next game
+// create a function where the available letters gray out when selected and the picked letters illuminate upon selection
+// when a selection is not in the phrase or word have a function populate part of the image
+// create an event for when the image is completed or phrase is completed for a loss or win situation respectively
+// create a reset button and function that appears after the game is over
+// reset button should revert back to word input phase while maintaining the player score
+// maybe use opacity to make the image appear upon incorrect responses and disappear upon reset
+// create a messages box that responds to user selections. e.g. "You have already selected ${letter}", "${letter} is not in the phrase", "There's ${numberOfLetters} ${letter} in the phrase! Good Job!", "You're getting close to takeoff... be careful!", etc
